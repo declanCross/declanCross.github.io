e7c4081158496034aa64ec6e73da3ac3d7523dfb
declanCross <declan_cross@outlook.com>
2025-06-30 15:36:37 +1000

    build and sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	siteUrl: "https://cross-development-services.vercel.app", // Replace with your actual domain
+	generateRobotsTxt: true,
+	outDir: "public", // or 'public' if that's where your static files go
+}
