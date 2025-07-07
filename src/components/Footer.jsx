4491a953bb82cc8843228173b49fbd0cd4a80d7a
declanCross <declan_cross@outlook.com>
2025-07-08 07:31:20 +1000

    add cds to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -88,6 +88,19 @@ export default function Footer() {
 				<p className="text-sm my-2 font-medium text-center">
 					<span>© 2025 Cross Development Services</span>
 				</p>
+				<p className="text-sm my-2 font-medium text-center">
+					<span>
+						Site design and build by{" "}
+						<Link
+							href="https://www.crossdevelopmentservices.com"
+							className="text-neutral-600 hover:text-secondary-blue transform transition-colors duration-300"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Cross Development Services
+						</Link>
+					</span>
+				</p>
 			</section>
 		</>
 	)
